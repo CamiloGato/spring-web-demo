39463f4b86be8b61c6842617ea8a4284c7b94b35
Camilo Andrés Rojas Hernández <93359519+Gaussito@users.noreply.github.com>
2022-06-10 21:11:58 -0500

    Starting with Spring

--- src/main/java/com/gaussito/springweb/controllers/IndexController.java
+++ src/main/java/com/gaussito/springweb/controllers/IndexController.java
@@ -1,12 +1,8 @@
 package com.gaussito.springweb.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Map;
 
 
 @Controller
