f9a41d10b3db560a9ed150801178f7345a392a1f
Camilo Andrés Rojas Hernández <93359519+Gaussito@users.noreply.github.com>
2022-06-15 16:39:13 -0500

    Starting with Spring

--- src/main/java/com/gaussito/springweb/controllers/IndexController.java
+++ src/main/java/com/gaussito/springweb/controllers/IndexController.java
@@ -2,13 +2,16 @@ package com.gaussito.springweb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-    @GetMapping(path = {"/","/index","/home"})
+    @GetMapping(path = {"","/","/index","/home"})
     public ModelAndView index(ModelAndView model){
         model.addObject("title","Hola Spring Framework with Model And View");
         model.setViewName("index");
