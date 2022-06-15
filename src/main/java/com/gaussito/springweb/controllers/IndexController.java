980492fb41427f631251139a522c81da9021ef69
Camilo Andrés Rojas Hernández <93359519+Gaussito@users.noreply.github.com>
2022-06-15 17:05:49 -0500

    Starting with Spring

--- src/main/java/com/gaussito/springweb/controllers/IndexController.java
+++ src/main/java/com/gaussito/springweb/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.gaussito.springweb.controllers;
 
+import com.gaussito.springweb.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,17 @@ public class IndexController {
     public ModelAndView index(ModelAndView model){
         model.addObject("title","Hola Spring Framework with Model And View");
         model.setViewName("index");
+        return model;
+    }
+
+    @RequestMapping(path = "/perfil", method = RequestMethod.GET)
+    public ModelAndView perfil(ModelAndView model){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Juan");
+        usuario.setApellido("Perez");
+        model.addObject("usuario", usuario);
+        model.addObject("title","Perfil de usuario: " + usuario.getNombre());
+        model.setViewName("perfil");
         return model;
     }
 
